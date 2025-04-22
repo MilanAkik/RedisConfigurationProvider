57b42829fd5a1a683bf0ebcb89c170d08111b74c
MilanAkik <mixa.mixony@gmail.com>
2025-04-23 00:35:23 +0200

    Added a get path to the webapi which tests the usage of the library

--- LibraryTestProject/Program.cs
+++ LibraryTestProject/Program.cs
@@ -1,6 +1,9 @@
+using RedisConfigurationProvider;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/{n}", (int n) => $"Hello World!\n[{string.Join(',',Class1.GetSquares(n))}]");
 
 app.Run();
