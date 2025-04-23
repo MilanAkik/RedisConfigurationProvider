77c55df7d60625d440e12755bbdb35e5317f003b
MilanAkik <mixa.mixony@gmail.com>
2025-04-23 02:02:58 +0200

    Added an implementation to the redis configuration provider that reads the configuration from static data depending on the key

--- RedisConfigurationProvider/Providers/RedisConfigurationProvider.cs
+++ RedisConfigurationProvider/Providers/RedisConfigurationProvider.cs
@@ -9,9 +9,16 @@ namespace RedisConfigurationProvider.Providers
 {
     public sealed class RedisConfigurationProvider(string? key) : ConfigurationProvider
     {
+
+        public static Dictionary<string, Dictionary<string, string>> DataSets = new Dictionary<string, Dictionary<string, string>> {
+            { "TestKey" , new Dictionary<string, string>{ { "Data1", "Value1" },  { "Data2", "Value2" } } },
+            { "ProdKey" , new Dictionary<string, string>{ { "Data1", "ProdValue1" },  { "Data2", "ProdValue2" } } }
+        };
+
         public override void Load()
         {
-            base.Load();
+            Dictionary<string, string> dataset = DataSets[key];
+            foreach (var item in dataset) Data.Add(item);
         }
     }
 }
