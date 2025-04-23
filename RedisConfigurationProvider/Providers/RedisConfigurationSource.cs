b3b3e783d4970bee4860647127afe9e8ffb679bd
MilanAkik <mixa.mixony@gmail.com>
2025-04-23 22:43:22 +0200

    Passing the connection string and key along to the provider

--- RedisConfigurationProvider/Providers/RedisConfigurationSource.cs
+++ RedisConfigurationProvider/Providers/RedisConfigurationSource.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace RedisConfigurationProvider.Providers
 {
-    public sealed class RedisConfigurationSource(string? key) : IConfigurationSource
+    public sealed class RedisConfigurationSource(string connectionString, string key) : IConfigurationSource
     {
-        public IConfigurationProvider Build(IConfigurationBuilder builder) => new RedisConfigurationProvider(key);
+        public IConfigurationProvider Build(IConfigurationBuilder builder) => new RedisConfigurationProvider(connectionString, key);
     }
 }
