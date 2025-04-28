d368f08317a10d986b201ec53fe7a634529bf828
MilanAkik <mixa.mixony@gmail.com>
2025-04-28 02:07:18 +0200

    Removed the leftover class1 and the call to it from the test api

--- RedisConfigurationProvider/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RedisConfigurationProvider
-{
-    public class Class1
-    {
-        public static IEnumerable<int> GetSquares(int n) => Enumerable.Range(1, n).Select(x => x * x);
-    }
-}
