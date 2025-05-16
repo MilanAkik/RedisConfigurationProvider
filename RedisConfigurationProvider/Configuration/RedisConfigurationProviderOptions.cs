6da1c5ae14c04d15a736f99e551876452e694d7b
MilanAkik <mixa.mixony@gmail.com>
2025-05-17 01:07:20 +0200

    Fixed a typo

--- RedisConfigurationProvider/Configuration/RedisConfigurationProviderOptions.cs
+++ RedisConfigurationProvider/Configuration/RedisConfigurationProviderOptions.cs
@@ -13,7 +13,7 @@ namespace RedisConfigurationProvider.Configuration
         public const string Name = "RedisConfigurationProvider";
 
         public string Url { get; set; } = "localhost";
-        public int Port { get; set; } = 9379;
+        public int Port { get; set; } = 6379;
         public string Username { get; set; } = "default";
         public string Password { get; set; } = "";
         public string Key { get; set; } = "";
