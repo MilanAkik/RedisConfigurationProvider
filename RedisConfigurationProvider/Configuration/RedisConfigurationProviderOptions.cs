59f06f083fd616a9bc6598490010dd471080cc5c
MilanAkik <mixa.mixony@gmail.com>
2025-05-17 00:53:16 +0200

    Added a class to represent the interim options of the provider

---
+++ RedisConfigurationProvider/Configuration/RedisConfigurationProviderOptions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisConfigurationProvider.Configuration
+{
+    public class RedisConfigurationProviderOptions
+    {
+
+        public const string Name = "RedisConfigurationProvider";
+
+        public string Url { get; set; } = "localhost";
+        public int Port { get; set; } = 9379;
+        public string Username { get; set; } = "default";
+        public string Password { get; set; } = "";
+        public string Key { get; set; } = "";
+
+    }
+}
