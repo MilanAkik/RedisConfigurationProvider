fb81050635438f22b11b597e4955adce909e79d7
MilanAkik <mixa.mixony@gmail.com>
2026-04-08 02:35:49 +0200

    Made logger factory parameters optional defaulting to null and if null instantiating a dummy logger factory inside of extension methods

--- LibraryTestProject/Program.cs
+++ LibraryTestProject/Program.cs
@@ -4,13 +4,7 @@ using LibraryTestProject.Services.Models;
 using RedisConfigurationProvider.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-using var loggerFactory = LoggerFactory.Create(loggingBuilder =>
-{
-    loggingBuilder.SetMinimumLevel(LogLevel.Warning);
-    loggingBuilder.AddFilter("RedisConfigurationProvider", LogLevel.Trace);
-    loggingBuilder.AddConsole();
-});
-builder.Configuration.AddRedisConfiguration(loggerFactory);
+builder.Configuration.AddRedisConfiguration();
 builder.Services.Configure<ConfigurationData>(builder.Configuration.GetSection(nameof(ConfigurationData)));
 builder.Services.Configure<OverrideTestData>(builder.Configuration.GetSection(nameof(OverrideTestData)));
 builder.Services.AddSingleton<IConfigurationTestService, ConfigurationTestService>();
--- RedisConfigurationProvider/Extensions/ConfigurationManagerExtensions.cs
+++ RedisConfigurationProvider/Extensions/ConfigurationManagerExtensions.cs
@@ -8,15 +8,26 @@ namespace RedisConfigurationProvider.Extensions
     public static class ConfigurationManagerExtensions
     {
 
-        public static ConfigurationManager AddRedisConfiguration(this ConfigurationManager configurationManager, ILoggerFactory loggerFactory)
+        public static ConfigurationManager AddRedisConfiguration(this ConfigurationManager configurationManager, ILoggerFactory? loggerFactory=null)
         {
+            loggerFactory ??= LoggerFactory.Create(loggingBuilder =>
+            {
+                loggingBuilder.SetMinimumLevel(LogLevel.Information);
+                loggingBuilder.AddConsole();
+            });
+            loggerFactory ??= LoggerFactory.Create(builder => builder.AddConsole());
             var options = configurationManager.GetSection(RedisConfigurationProviderOptions.Name).Get<RedisConfigurationProviderOptions>();
             IConfigurationBuilder builder = configurationManager;
             builder.Add(new RedisConfigurationSource(options, loggerFactory));
             return configurationManager;
         }
-        public static ConfigurationManager AddRedisConfiguration(this ConfigurationManager configurationManager, Action<RedisConfigurationProviderOptions> setupOptions, ILoggerFactory loggerFactory)
+        public static ConfigurationManager AddRedisConfiguration(this ConfigurationManager configurationManager, Action<RedisConfigurationProviderOptions> setupOptions, ILoggerFactory? loggerFactory = null)
         {
+            loggerFactory ??= LoggerFactory.Create(loggingBuilder =>
+            {
+                loggingBuilder.SetMinimumLevel(LogLevel.Information);
+                loggingBuilder.AddConsole();
+            });
             IConfigurationBuilder builder = configurationManager;
             var options = configurationManager.GetSection(RedisConfigurationProviderOptions.Name).Get<RedisConfigurationProviderOptions>();
             setupOptions(options);
